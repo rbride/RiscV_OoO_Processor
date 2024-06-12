2683a9a43fa9c7519596bc18654e9b30e1ce31fb
rbride <rbride@mail.bradley.edu>
2024-06-11 23:18:54 -0400

    added ISR file and began work

---
+++ project2/ISR.sv
@@ -0,0 +1,6 @@
+/* Integer square root calculator that works in tandem with the 8 stage multiply to calculate
+the highest 32 bit integer that is closest but still less than the square root of the 
+provided 64 bit number... I.E highest integer fufilling the following provide > result * result
+Also required to take less than 600 cycles
+*/
+
